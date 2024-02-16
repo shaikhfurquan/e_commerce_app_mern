fc1f98e4601ad8ea42e24888b34c9d414db2cd28
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-16 13:46:42 +0530

    json/morgan setup

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import morgan from 'morgan';
 import connectDB from './db/connectDB.js';
 
 dotenv.config()
 
 const app = express()
 
+//express middleware
+app.use(express.json())
+app.use(morgan('dev'))
 
-
+ 
 //db connection
-connectDB()
+connectDB() 
 
 app.listen(process.env.PORT , ()=>{
     console.log(`Server started at ${process.env.PORT}`);
