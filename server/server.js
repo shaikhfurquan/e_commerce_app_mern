6e7a06b32aa469a4432b6bbd8c2a63cef5248380
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-17 01:07:41 +0530

    cors & user router

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv'
 import morgan from 'morgan';
 import connectDB from './db/connectDB.js';
+import userRouter from './routes/userRoute.js';
+import cors from 'cors'
+
 
 dotenv.config()
 
@@ -10,8 +13,12 @@ const app = express()
 //express middleware
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(cors())
 
  
+//routes
+app.use('/api/user' , userRouter)
+
 //db connection
 connectDB() 
 
