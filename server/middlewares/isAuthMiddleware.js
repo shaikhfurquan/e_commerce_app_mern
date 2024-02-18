a57fa52ee40d285e09c61361cc006bf037bbacd8
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-18 14:03:46 +0530

    token comment

--- server/middlewares/isAuthMiddleware.js
+++ server/middlewares/isAuthMiddleware.js
@@ -1,7 +1,8 @@
 
 import JWT from 'jsonwebtoken'
-import UserModel from '../models/userModel.js'
 
+
+// protected routes using JWT token
 export const isAuthenticated = async (req, res, next) => {
     try {
         // const token = req.headers.authorization;
@@ -28,35 +29,3 @@ export const isAuthenticated = async (req, res, next) => {
 }
 
 
-//Admin access
-
-
-export const isAdmin = async (req, res, next) => {
-    try {
-
-        let id = req.user._id;
-        const user = await UserModel.findById({_id : id});
-        
-        if(!user) {
-            return res.status(404).json({
-                success: false,
-                message: "User not found",
-            })
-        }
-
-        if (user.role !== 'admin') {
-            return res.status(401).json({
-                success: false,
-                message: "Unauthorized Access, Only admin can access"
-            });
-        } else {
-            next();
-        }
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: "isAdmin middleware error",
-            error: error.message
-        });
-    }
-};
