b51eaabf6c212df88b2ccaad950d80d669883097
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-18 14:04:34 +0530

    test route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,19 @@
 
 import express from 'express'
 import { loginUser, registerUser } from '../controllers/userController.js';
+import { isAuthenticated } from '../middlewares/isAuthMiddleware.js';
+import { isAdmin } from '../middlewares/isAdminMiddleware.js';
+
 
 const userRouter = express.Router();
 
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
+
+
+userRouter.get('/get', isAuthenticated , isAdmin ,  (req, res)=>{
+    res.json({message : "Welcome"})
+});
 
 
 export default userRouter
