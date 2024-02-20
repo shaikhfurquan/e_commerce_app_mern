1f67a37a5c41989c94873ad7d9ebafdc37332339
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-21 01:16:57 +0530

    forget/update password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,8 +4,8 @@ import UserModel from "../models/userModel.js"
 
 export const registerUser = async (req, res) => {
     try {
-        const { name, email, password, phone, address } = req.body
-        if (!name || !email || !password || !phone || !address) {
+        const { name, email, password, phone, address, answer } = req.body
+        if (!name || !email || !password || !phone || !address || !answer) {
             return res.status(404).json({
                 success: false,
                 message: "Please provide all fields"
@@ -22,7 +22,7 @@ export const registerUser = async (req, res) => {
 
         //registration of user(hashing the password from the req.body)
         const hashedPassword = await hashPassword(password)
-        const newUser = await UserModel.create({ name, email, phone, address, password: hashedPassword })
+        const newUser = await UserModel.create({ name, email, phone, address,answer ,password: hashedPassword })
 
         res.status(201).json({
             success: true,
@@ -52,19 +52,19 @@ export const loginUser = async (req, res) => {
         }
         //check wheather the user exists or not
         const user = await UserModel.findOne({ email })
-        if (!user){
+        if (!user) {
             return res.status(400).json({
                 success: false,
-                message : "User does not exist, Register first"
+                message: "User does not exist, Register first"
             })
         }
 
         //if user is there then we will compare the password(userPassword & insideDB user.password)
-        const isMath = await comparePassword(password , user.password)
-        if(!isMath){
+        const isMath = await comparePassword(password, user.password)
+        if (!isMath) {
             return res.status(400).json({
                 success: false,
-                message : "Invalid Credentials"
+                message: "Invalid Credentials"
             })
         }
         const token = await generateToken(user)
@@ -72,9 +72,9 @@ export const loginUser = async (req, res) => {
 
         res.status(200).json({
             success: true,
-            message : `Welcome ${user.name}`,
-            user : user,
-            token : token
+            message: `Welcome ${user.name}`,
+            user: user,
+            token: token
         })
     } catch (error) {
         res.status(500).json({
@@ -88,11 +88,80 @@ export const loginUser = async (req, res) => {
 
 export const forgotPassword = async (req, res) => {
     try {
-        const {email, question, newPassword} = req.body 
+        const { email, answer, newPassword } = req.body
+        if (!email || !newPassword || !answer) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+        //check email and answer if these are true then we will allow to update/change password
+        const user = await UserModel.findOne({ email , answer })
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message : "Wrong email or answer"
+            })
+        }
+        const hashed = await hashPassword(newPassword)
+        await UserModel.findByIdAndUpdate(user._id, {password: hashed})
+        res.status(200).json({
+            success: true,
+            message : "Password Reset successfully"
+        })
     } catch (error) {
         res.status(500).json({
             success: false,
             message: "Something went wrong",
+            error: error.message
+        })
+    }
+}
+
+
+export const updatePassword = async (req, res) => {
+    try {
+        //finding user on the basis of id
+        const user = await UserModel.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found with this id"
+            })
+        }
+
+        //getting the data from the req.body
+        const { oldPassword, newPassword } = req.body
+        if (!oldPassword || !newPassword) {
+            return res.status(404).json({
+                success: false,
+                message: "Please provide OldPassword and NewPassword"
+            })
+        }
+
+        //compare the oldPassword and userPassword
+        const isMatch = await comparePassword(oldPassword, user.password)
+        if (!isMatch) {
+            return res.status(500).json({
+                success: false,
+                message: "Invalid old password"
+            })
+        }
+
+        //hashing the password
+        const updateHashPassword = await hashPassword(newPassword, 10)
+        user.password = updateHashPassword
+        await user.save()
+
+        res.status(200).json({
+            success: true,
+            message: "Password updated successfully"
+        })
+    } catch (error) {
+        res.status(500).json({
+            succcess: false,
+            message: "Error while updating user password",
             error: error.message
         })
     }
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,6 @@
 
 import express from 'express'
-import { forgotPassword, loginUser, registerUser } from '../controllers/userController.js';
+import { forgotPassword, loginUser, registerUser, updatePassword } from '../controllers/userController.js';
 import { isAuthenticated } from '../middlewares/isAuthMiddleware.js';
 import { isAdmin } from '../middlewares/isAdminMiddleware.js';
 
@@ -11,6 +11,7 @@ userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
 
 userRouter.post('/forgot-password', forgotPassword);
+userRouter.post('/update-password/:id', updatePassword);
 
 // userRouter.get('/get', isAuthenticated , isAdmin ,  (req, res)=>{
 //     res.json({message : "Welcome"})
