d92d49c69118965d056276d0ea4bf2aec5958ade
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-22 01:38:13 +0530

    express-formidable

---
+++ server/controllers/productController.js
@@ -0,0 +1,68 @@
+import ProductModel from '../models/productModel.js'
+
+
+export const createProduct = async (req, res) =>{
+    try {
+        await ProductModel.create()
+    } catch (error) {
+        res.status(500).json({
+            success : false,
+            message : "Error creating product",
+            error : error.message
+        })
+    }
+}
+
+
+
+export const getProduct = async (req, res) =>{
+    try {
+        
+    } catch (error) {
+        res.status(500).json({
+            success : false,
+            message : "Error getting  products",
+            error : error.message
+        })
+    }
+}
+
+
+export const getSingleProduct = async (req, res) =>{
+    try {
+        
+    } catch (error) {
+        res.status(500).json({
+            success : false,
+            message : "Error creating product",
+            error : error.message
+        })
+    }
+}
+
+
+export const updateProduct = async (req, res) =>{
+    try {
+        
+    } catch (error) {
+        res.status(500).json({
+            success : false,
+            message : "Error updating product",
+            error : error.message
+        })
+    }
+}
+
+
+export const deleteProduct = async (req, res) =>{
+    try {
+        
+    } catch (error) {
+        res.status(500).json({
+            success : false,
+            message : "Error deleting product",
+            error : error.message
+        })
+    }
+}
+
--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import { isAuthenticated } from '../middlewares/isAuthMiddleware.js';
+import { isAdmin } from '../middlewares/isAdminMiddleware.js';
+import { createProduct } from '../controllers/productController.js';
 
 const productRouter = express.Router();
 
+productRouter.post('/create-product' , isAuthenticated , isAdmin , createProduct)
 
 
 export default productRouter
