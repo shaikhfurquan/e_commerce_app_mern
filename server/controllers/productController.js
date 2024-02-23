5f843e24df3237be6f2315c27c5e34383ea1eea3
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-23 17:48:13 +0530

    create product ctrl

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,66 +1,95 @@
+import { error } from 'console'
 import ProductModel from '../models/productModel.js'
+import fs from 'fs'
+import slugify from 'slugify'
 
 
-export const createProduct = async (req, res) =>{
+export const createProduct = async (req, res) => {
     try {
-        await ProductModel.create()
+        const { name, slug, description, price, category, quantity, shipping } = req.fields
+        const { photo } = req.files
+
+        if (!name || !description || !price || !category || !quantity ) {
+            return res.status(404).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+        if (photo && photo.size > 1000000) {
+            return res.status(404).json({
+                message: "Photo is required & less than 1MB"
+            })
+        }
+        //creating one copy of product before saving
+        const newProduct = ProductModel({ ...req.fields, slug: slugify(name) })
+        if (photo) {
+            newProduct.photo.data = fs.readFileSync(photo.path)
+            newProduct.photo.contentType = photo.type
+        }
+        await newProduct.save()
+        res.status(201).json({
+            success: true,
+            message: "Product created successfully",
+            newProduct: newProduct
+        })
     } catch (error) {
         res.status(500).json({
-            success : false,
-            message : "Error creating product",
-            error : error.message
+            success: false,
+            message: "Error creating product",
+            error: error.message
         })
     }
 }
 
 
-export const getProduct = async (req, res) =>{
+export const getProduct = async (req, res) => {
     try {
-        
+
     } catch (error) {
         res.status(500).json({
-            success : false,
-            message : "Error getting  products",
-            error : error.message
+            success: false,
+            message: "Error getting  products",
+            error: error.message
         })
     }
 }
 
 
-export const getSingleProduct = async (req, res) =>{
+export const getSingleProduct = async (req, res) => {
     try {
-        
+
     } catch (error) {
         res.status(500).json({
-            success : false,
-            message : "Error creating product",
-            error : error.message
+            success: false,
+            message: "Error creating product",
+            error: error.message
         })
     }
 }
 
 
-export const updateProduct = async (req, res) =>{
+export const updateProduct = async (req, res) => {
     try {
-        
+
     } catch (error) {
         res.status(500).json({
-            success : false,
-            message : "Error updating product",
-            error : error.message
+            success: false,
+            message: "Error updating product",
+            error: error.message
         })
     }
 }
 
 
-export const deleteProduct = async (req, res) =>{
+export const deleteProduct = async (req, res) => {
     try {
-        
+
     } catch (error) {
         res.status(500).json({
-            success : false,
-            message : "Error deleting product",
-            error : error.message
+            success: false,
+            message: "Error deleting product",
+            error: error.message
         })
     }
 }
